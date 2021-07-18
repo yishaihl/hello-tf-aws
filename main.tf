f380503b14ba978f26116f70053e3dc479c4d690
yishaihl <[EMAIL_ADDRESS]>
2021-07-18 19:40:44 +0300

    added kubernetes provider

--- main.tf
+++ main.tf
@@ -89,10 +89,10 @@
         },
         ]
         worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
-        provider "kubernetes" {
+    }
+    provider "kubernetes" {
             host = data.aws_eks_cluster.cluster.endpoint
             cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
             token = data.aws_eks_cluster_auth.cluster.token
             load_config_file = false
         }
-    }
