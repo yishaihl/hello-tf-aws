3f041cf224509ac29233da78284ca5d1be90ebee
yishaihl <[EMAIL_ADDRESS]>
2021-07-18 19:46:18 +0300

    final fix

--- main.tf
+++ main.tf
@@ -91,8 +91,9 @@
         worker_additional_security_group_ids = [aws_security_group.all_worker_mgmt.id]
     }
     provider "kubernetes" {
-            host = data.aws_eks_cluster.cluster.endpoint
+            host                   = data.aws_eks_cluster.cluster.endpoint
             cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-            token = data.aws_eks_cluster_auth.cluster.token
-            load_config_file = false
+            token                  = data.aws_eks_cluster_auth.cluster.token
+            load_config_file       = false
+            version                = "~> 1.11"
         }
